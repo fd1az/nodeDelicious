d0cbc300b8cc835744d0d097c54594af430faf3d
fdiaz7 <facu.diaz777@gmail.com>
2018-11-27 22:44:49 -0300

    user registrer and log in

---
+++ controllers/authController.js
@@ -0,0 +1,10 @@
+const passport = require('passport');
+
+
+exports.login = passport.authenticate('local',{
+    failureRedirect:'/login',
+    failureFlash:'Failed login',
+    successRedirect:'/',
+    successFlash:'You are logged in'
+})
+
--- models/User.js
+++ models/User.js
@@ -23,6 +23,6 @@ const userSchema = new Schema({
 });
 
 userSchema.plugin(passportLocalMongoose,{usernameField:'email'});
-userSchema.plugin(mongodbErrorHandler,)
+userSchema.plugin(mongodbErrorHandler)
 
 module.exports = mongoose.model('User',userSchema);
